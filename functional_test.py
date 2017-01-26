a68fb869d02270e85ef657daa9ee3a6a1be69ed1
fido2478 <fido2478@gmail.com>
2017-01-26 13:55:12 +0900

    템플릿을 이용한 페이지 HTML 생성

--- functional_test.py
+++ functional_test.py
@@ -24,9 +24,10 @@ class NewVisitorTest(unittest.TestCase):
         inputbox.send_keys(Keys.ENTER)
         table = self.browser.find_element_by_id('id_list_table')
         rows = table.find_elements_by_tag_name('tr')
-        self.assertTrue(any(row.text == '1: 공작깃털 사기' for row in frows), )
+        self.assertTrue(any(row.text == '1: 공작깃털 사기' for row in rows),
+                        "신규 작업이 테이블에 표시되지 않음" )
 
-        self.fail('Finish the test!')
+        #self.fail('Finish the test!')
 
 if __name__ == '__main__':
     unittest.main(warnings='ignore')
